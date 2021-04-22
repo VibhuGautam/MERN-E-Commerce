9c87902eeed1ec55fd1d73d9a84466bd5ebf121a
VibhuGautam <vibhuparmar13@gmail.com>
2021-04-22 18:52:08 +0530

    User Signup

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
     //import routes
 const userRoutes = require('./routes/user')
@@ -13,6 +16,10 @@ mongoose.connect(process.env.DATABASE, {
     useCreateIndex: true
 }).then(() => console.log('DB Connected'))
 
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 //route middleware
 app.use('/api', userRoutes)
