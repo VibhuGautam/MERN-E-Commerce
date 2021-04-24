a11919732eb154220f996b777eaea228c926b100
VibhuGautam <vibhuparmar13@gmail.com>
2021-04-24 13:15:15 +0530

    Auth & Admin Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,3 +59,25 @@ exports.requireSignin = expressJwt({
     algorithms: ["HS256"],
     userProperty: "auth",
 });
+
+
+
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id
+    if (!user) {
+        return res.status(400).json({
+            error: "Access Denied"
+        })
+    }
+
+    next()
+}
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: 'Admin resource ! Acess denied'
+        })
+    }
+    next();
+}
