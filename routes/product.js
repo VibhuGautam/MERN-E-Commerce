f5f48b2b308b9d5b7c958da1d22d5f205dad6bfb
VibhuGautam <vibhuparmar13@gmail.com>
2021-04-25 11:52:12 +0530

    Create Product Model

---
+++ routes/product.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+
+const { create } = require('../controllers/prodcut')
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+
+
+router.post('/prodcut/create/:userId',
+    requireSignin,
+    isAuth,
+    isAdmin,
+    create);
+
+
+router.param("userId", userById);
+module.exports = router;
