61c76a9673147b5c09a3da75e04f1c1b000765f1
VibhuGautam <vibhuparmar13@gmail.com>
2021-04-27 19:29:46 +0530

    Sending Products

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 
-const { create, productById, read, remove, update, list } = require('../controllers/product')
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require('../controllers/product')
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -25,6 +25,11 @@ router.put('/product/:productId/"userId',
     isAdmin, update)
 
 router.get('/products', list)
+router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.post('/products/by/search', listBySearch)
+router.get('/products/photo/:productId', photo)
+
 
 router.param("userId", userById);
 
